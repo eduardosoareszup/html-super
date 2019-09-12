40a71ea82244a77fcb2b3b95ac3049af81dbb500
Eduardo Soares <eduardo.soares@zup.com.br>
2019-09-12 17:07:31 -0300

    Novo exemplo de arquivo html-super

---
+++ examples/html-super/simple.js
@@ -0,0 +1,40 @@
+const Home = (username = 'usuário') => {
+  secondaryTitle = 'Meu título secundário';
+  
+  return {
+    head: {},
+    body: {
+      children: [
+        {
+          main: {
+            props: {
+              id: 'content',
+              class: 'container'
+            },
+            children: [
+              {
+                div: {
+                  props: { class: 'row' },
+                  children: [
+                    {
+                      div: {
+                        props: { class: 'col s6' },
+                        children: [{ h1: `Olá ${username}! Seja bem-vindo` }]
+                      }
+                    },
+                    {
+                      div: {
+                        props: { class: 'col s6' },
+                        children: [{ h2: secondaryTitle }]
+                      }
+                    },
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+}
